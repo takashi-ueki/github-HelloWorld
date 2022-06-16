28b24abd29026bc977b9465f66bea77f56242993
takashi-ueki <takashiinjapan1982@gmail.com>
2022-06-16 15:51:26 +0900

    第１回課題

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            if (i < 10) {
+                System.out.println("Hello!, World");
+            } else {
+                System.out.println("Hello");
+            }
+        }
+    }
+}
+
